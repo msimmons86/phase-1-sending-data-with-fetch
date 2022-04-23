e0565a6bfa11fa8b81edced7b3f87de64e84ea71
Michael Simmons <msimmons86@gmail.com>
2022-04-23 15:15:05 -0700

    commit tree 65e95c706cc777c4834c97e1c7f105ededdb0e3f

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+
+function submitData(firstName, email){
+  const userDetails = {
+    method:'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body:JSON.stringify({
+      "name":firstName,
+      "email":email
+    })
+  };
+  return fetch('http://localhost:3000/users', userDetails)
+  .then(res => res.json())
+  .then(body => newUser(body.id))
+  .catch(error => errorMessage())
+}
+function newUser(newID){
+  document.getElementsByTagName("body")[0].innerHTML = newID;
+}
+
+function errorMessage(error){
+  document.body.innerHTML = `Unauthorized Access`
+}
+
+submitData("", "")
+
